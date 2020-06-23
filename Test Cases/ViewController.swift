945783f05d4961b3702e9789957b5b976cb0cab5
Shafeer P <shafeer.p@redbus.com>
2020-06-23 21:18:45 +0530

    Initial Commit

---
+++ Test Cases/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test Cases
+//
+//  Created by Shafeer P on 23/06/20.
+//  Copyright © 2020 Shafeer P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test CasesTests/Test_CasesTests.swift
@@ -0,0 +1,34 @@
+//
+//  Test_CasesTests.swift
+//  Test CasesTests
+//
+//  Created by Shafeer P on 23/06/20.
+//  Copyright © 2020 Shafeer P. All rights reserved.
+//
+
+import XCTest
+@testable import Test_Cases
+
+class Test_CasesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
